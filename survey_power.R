62612d598780524df11feb2f44d887e0930ca040
Michael Feurstein <michael.feurstein@wu.ac.at>
2025-07-09 16:42:04 +0200

    add post-hoc power analysis for friedman
    - added posthoc power calculation for Friedman test
    - added sources for this calculation and threats to validity for
      Friedman

--- survey_power.R
+++ survey_power.R
@@ -9,4 +9,11 @@ cohen.ES(test = "anov", size = "medium")
 # medium effect size (0.25)
 # significance level 0.05
 # 80% power
-pwr.anova.test(k = 3, f = 0.25, sig.level = 0.05, power = 0.80)
+power_anova <- pwr.anova.test(k = 3, f = 0.25, sig.level = 0.05, power = 0.80)
+
+# after study / power analysis for Friedman test (post-hoc calculation of power)
+# source: 
+# - https://www.statisticssolutions.com/wp-content/uploads/wp-post-to-pdf-enhanced-cache/1/friedman-anova-7-dependent-variables.pdf
+# - Lehmann: Lehmann, E.L. (2006) Nonparametrics: Statistical methods based on ranks. New York, NY: Springer.
+samplesize_friedman <- power_anova$n * 1.15
+samplesize_friedman
